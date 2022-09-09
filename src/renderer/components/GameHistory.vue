f1827e15570059e9f283c8bb7967f27dd38c79a1
arrebole <326093153@qq.com>
2022-09-09 18:52:31 +0800

    style: 美化滚动条样式

--- src/renderer/components/GameHistory.vue
+++ src/renderer/components/GameHistory.vue
@@ -1,15 +1,10 @@
 <template>
-    <div 
-        class="border-gray-400 z-10 border absolute top-0 left-0 bottom-0 right-0 m-auto bg-black divide-y divide-x overflow-y-scroll"
-        style="height: 300px; width: 300px;"
-    >
-        <div 
-            v-for="game in games" :key="game.gameId"
-            class="flex divide-x"
-        >
-            <span class="p-1 w-20">{{ game.champion  }} </span>
-            <span class="p-1 w-10">{{ game.win  }} </span>
-            <span class="p-1 w-20">{{ game.gameType  }} </span>
+    <div class="border-gray-400 z-10 border absolute top-0 left-0 bottom-0 right-0 m-auto bg-black divide-y divide-x overflow-y-scroll scroll-container"
+        style="height: 300px; width: 300px;">
+        <div v-for="game in games" :key="game.gameId" class="flex divide-x">
+            <span class="p-1 w-20">{{ game.champion }} </span>
+            <span class="p-1 w-10">{{ game.win }} </span>
+            <span class="p-1 w-20">{{ game.gameType }} </span>
             <div class="p-1">
                 <span>{{ game.kills }}</span>
                 <span>/</span>
@@ -28,11 +23,11 @@ import { queueIdToGameZhType } from '../../shared/queue';
 import { findChampion } from '../../shared/champions';
 
 interface Props {
-  games: Game[]
+    games: Game[]
 }
 const propos = defineProps<Props>();
 
-const games = propos.games.map((v)=>({
+const games = propos.games.map((v) => ({
     gameId: v.gameId,
     gameType: queueIdToGameZhType(v.queueId),
     champion: findChampion(v.participants[0].championId)?.label,
